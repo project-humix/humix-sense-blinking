5a082333f188e0194bf93a16cd31c15d49a6c062
YI-HONG WANG <yhwang@tw.ibm.com>
2015-05-13 20:36:27 +0800

    add blinking.py for other 2 humix

---
+++ blinking-4-big.py
@@ -0,0 +1,53 @@
+import time, random, sys
+import RPi.GPIO as GPIO
+
+operation = "blink"
+speed = 1
+ang0 = 7.6
+ang90 = 4
+ang180 = 2.7
+initDC = ang0
+needCycle = False
+
+#check operation command first
+argnum = len(sys.argv)
+if argnum > 4 or argnum < 3:
+    print "usage : %s GPIO_PIN 'blink|half|close|open'" %(sys.argv[0])
+    sys.exit(-1)
+
+GPIONum = int(sys.argv[1])
+operation = sys.argv[2]
+
+if operation == "blink" or operation == "helf":
+    if argnum == 4:
+        speed = int(sys.argv[3])
+
+if speed > 3:
+    speed = 3
+    
+if operation == "close":
+    initDC = ang180
+elif operation == "half":
+    initDC = ang90
+elif operation == "open":
+    initDC = ang0
+elif operation == "blink":
+    needCycle = True
+    
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIONum, GPIO.OUT)
+p = GPIO.PWM(GPIONum, 50)  #frequency=50Hz
+try:
+    p.start(initDC)
+    time.sleep(0.75)
+    if needCycle == True:
+        p.ChangeDutyCycle(ang180)
+        time.sleep( (float)(speed)/10 + 0.2 )
+        p.ChangeDutyCycle(ang0)
+        time.sleep(1)
+
+    p.stop()
+    GPIO.cleanup()
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
